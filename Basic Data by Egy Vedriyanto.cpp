7491b23cf6d23221628f9ab465ad60d0d75c6bb6
Egy Vedriyanto <90687836+egyvedriyanto78@users.noreply.github.com>
2021-11-24 13:50:45 +0700

    Add files via upload

---
+++ Basic Data by Egy Vedriyanto.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+/* NAMA   : EGY VEDRIYANTO
+   NPM    : 2117051035
+   KELAS  : B
+*/
+
+// ini merupakan program untuk membuat data basic BIODATA PELANGGAN
+int main(){
+	string a, b, c, d;
+	
+	cout<<"--------------->>> BIODATA PELANGGAN <<<---------------"<<endl<<""<<endl;
+	cout<<"Masukkan data dengan urutan Nama Pelanggan, Alamat, Email, dan Nomor telepon."<<endl;
+	cout<<"Contoh :"<<endl<<"Eren Yeager"<<endl<<"Natar"<<endl<<"Eren123@gmail.com"<<endl<<"089512345678"<<endl;
+	cout<<""<<endl;
+	cout<<"<Masukkan Biodata Pelanggan>"<<endl<<""<<endl;
+	
+	
+	//Input
+	cout<<"";
+	getline(cin,a);
+	cout<<"";
+	getline(cin,b);
+	cout<<"";
+	getline(cin,c);
+	cout<<"";
+	getline(cin,d);
+	
+	//Output
+		cout<<""<<endl;
+		cout<<"--------------->>> BIODATA PELANGGAN <<<---------------"<<endl;
+		cout<<""<<endl;
+	cout<<"Nama Pelanggan  : "<<a<<endl;
+	cout<<"Alamat Rumah    : "<<b<<endl;
+	cout<<"Email Pelanggan : "<<c<<endl;
+	cout<<"Nomor Telepon   : "<<d<<endl;
+	
+	return 0;
+}
